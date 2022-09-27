31c041b00f3b4139029ab37ddbdab2e51278e1d6
psp <976477689@qq.com>
2022-09-27 21:51:15 +0800

    late

--- src/main.ts
+++ src/main.ts
@@ -1,49 +1,74 @@
-import './style.css'
 interface IGrid {
   x: number,
-  y: number,
-  state: number
+  y: number
 }
-class Game {
+export default class Game {
   el: HTMLElement
-  size: number
+  boardSize: number
   gridNum: number
+  gridSize: number
   speed: number
-  board: IGrid[][] = []
-  constructor(el: HTMLElement, size: number = 500, gridNum: number = 25, speed: number = 1) {
+  board: HTMLCanvasElement = document.createElement('canvas')
+  ctx: CanvasRenderingContext2D = this.board.getContext('2d')!
+  snake: IGrid[] = []
+  constructor(el: HTMLElement, boardSize: number = 500, gridNum: number = 25, speed: number = 1000) {
     this.el = el
-    this.size = size
+    this.boardSize = boardSize
     this.gridNum = gridNum
+    this.gridSize = boardSize / gridNum
     this.speed = speed
     this.init()
   }
   init() {
     this.createLayout()
     this.createFood()
+    // this.createSnake()
+    this.snakeMove()
   }
   createLayout() {
-    this.el.classList.add('board')
-    this.el.style.width = this.size + 'px'
-    this.el.style.height = this.size + 'px'
+    this.board.width = this.board.height = this.boardSize
+    this.el.appendChild(this.board)
+    this.ctx.strokeRect(0, 0, this.boardSize, this.boardSize)
     for (let i = 0; i < this.gridNum; i++) {
-      const rowDivDom = document.createElement('div')
-      rowDivDom.classList.add('row')
-      rowDivDom.style.height = this.size / this.gridNum + 'px'
+      this.drawLine(0, this.gridSize * i, this.boardSize, this.gridSize * i)
       for (let j = 0; j < this.gridNum; j++) {
-        const colDivDom = document.createElement('div')
-        colDivDom.classList.add('col')
-        colDivDom.style.width = this.size / this.gridNum + 'px'
-        colDivDom.style.height = this.size / this.gridNum + 'px'
-        rowDivDom.appendChild(colDivDom)
+        this.drawLine(this.gridSize * j, 0, this.gridSize * j, this.boardSize)
       }
-      this.el.appendChild(rowDivDom)
     }
   }
   createFood() {
-    const randomX = Math.floor(Math.random() * this.gridNum)
-    const randomY = Math.floor(Math.random() * this.gridNum)
-    const rows = document.querySelectorAll('.row')
-    Array.from(rows)[randomX].children[randomY].classList.add('food')
+    const randomX = Math.floor(Math.random() * this.gridNum) * this.gridSize
+    const randomY = Math.floor(Math.random() * this.gridNum) * this.gridSize
+    this.ctx.fillStyle = 'red'
+    this.drawRect(randomX, randomY)
+  }
+  createSnake(len: number) {
+    for (let i = 0; i < len; i++) {
+      this.snake.push({
+        x: this.gridSize * i,
+        y: 0
+      })
+    }
+    this.ctx.fillStyle = 'black'
+    this.snake.forEach(item => {
+      this.drawRect(item.x, item.y)
+    })
+  }
+  snakeMove() {
+    // this.ctx.clearRect(0, 0, this.boardSize, this.boardSize)
+    this.snake = this.snake.map(item => {
+      item.x = item.x + this.gridSize
+      return item
+    })
+    this.createSnake(2)
+  }
+  drawLine(x: number, y: number, x1: number, y1: number) {
+    this.ctx.beginPath()
+    this.ctx.moveTo(x, y)
+    this.ctx.lineTo(x1, y1)
+    this.ctx.stroke()
+  }
+  drawRect(x: number, y: number) {
+    this.ctx.fillRect(x, y, this.gridSize, this.gridSize)
   }
 }
-export default Game
