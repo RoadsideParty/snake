c33cc0d33d39090e5f6140957a06b5b1a2c466dc
psp <976477689@qq.com>
2022-09-28 18:28:41 +0800

    commit

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,6 @@ export default class Game {
   gridNum: number
   gridSize: number
   speed: number
-  board: HTMLCanvasElement = document.createElement('canvas')
-  ctx: CanvasRenderingContext2D = this.board.getContext('2d')!
   snake: IGrid[] = []
   constructor(el: HTMLElement, boardSize: number = 500, gridNum: number = 25, speed: number = 1000) {
     this.el = el
@@ -21,26 +19,28 @@ export default class Game {
   }
   init() {
     this.createLayout()
-    this.createFood()
-    // this.createSnake()
-    this.snakeMove()
   }
   createLayout() {
-    this.board.width = this.board.height = this.boardSize
-    this.el.appendChild(this.board)
-    this.ctx.strokeRect(0, 0, this.boardSize, this.boardSize)
+    const canvas = document.createElement('canvas')
+    canvas.width = canvas.height = this.boardSize
+    this.el.appendChild(canvas)
+    const ctx = canvas.getContext('2d')!
+    ctx.strokeRect(0, 0, this.boardSize, this.boardSize)
     for (let i = 0; i < this.gridNum; i++) {
-      this.drawLine(0, this.gridSize * i, this.boardSize, this.gridSize * i)
+      this.drawLine(ctx, 0, this.gridSize * i, this.boardSize, this.gridSize * i)
       for (let j = 0; j < this.gridNum; j++) {
-        this.drawLine(this.gridSize * j, 0, this.gridSize * j, this.boardSize)
+        this.drawLine(ctx, this.gridSize * j, 0, this.gridSize * j, this.boardSize)
       }
     }
+    this.createFood(ctx)
+    this.createSnake(3)
+    // this.snakeMove()
   }
-  createFood() {
+  createFood(ctx: CanvasRenderingContext2D) {
     const randomX = Math.floor(Math.random() * this.gridNum) * this.gridSize
     const randomY = Math.floor(Math.random() * this.gridNum) * this.gridSize
-    this.ctx.fillStyle = 'red'
-    this.drawRect(randomX, randomY)
+    ctx.fillStyle = 'red'
+    this.drawRect(ctx, randomX, randomY)
   }
   createSnake(len: number) {
     for (let i = 0; i < len; i++) {
@@ -49,26 +49,26 @@ export default class Game {
         y: 0
       })
     }
-    this.ctx.fillStyle = 'black'
+    ctx.fillStyle = 'black'
     this.snake.forEach(item => {
-      this.drawRect(item.x, item.y)
+      this.drawRect(ctx, item.x, item.y)
     })
   }
-  snakeMove() {
-    // this.ctx.clearRect(0, 0, this.boardSize, this.boardSize)
-    this.snake = this.snake.map(item => {
-      item.x = item.x + this.gridSize
-      return item
-    })
-    this.createSnake(2)
+  // snakeMove() {
+  //   ctx.clearRect(0, 0, this.boardSize, this.boardSize)
+  //   this.snake = this.snake.map(item => {
+  //     item.x = item.x + this.gridSize
+  //     return item
+  //   })
+  //   this.createSnake()
+  // }
+  drawLine(ctx: CanvasRenderingContext2D, x: number, y: number, x1: number, y1: number) {
+    ctx.beginPath()
+    ctx.moveTo(x, y)
+    ctx.lineTo(x1, y1)
+    ctx.stroke()
   }
-  drawLine(x: number, y: number, x1: number, y1: number) {
-    this.ctx.beginPath()
-    this.ctx.moveTo(x, y)
-    this.ctx.lineTo(x1, y1)
-    this.ctx.stroke()
-  }
-  drawRect(x: number, y: number) {
-    this.ctx.fillRect(x, y, this.gridSize, this.gridSize)
+  drawRect(ctx: CanvasRenderingContext2D, x: number, y: number) {
+    ctx.fillRect(x, y, this.gridSize, this.gridSize)
   }
 }
